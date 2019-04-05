f5b00eaf42db1c31953a1b6dc91b0eab762b7a0e
Satgawas007 <48801433+Satgawas007@users.noreply.github.com>
2019-04-05 16:15:40 +0530

    Updates

---
+++ precache-manifest.ea9778a5d7eaeaef65947be9de23bef0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7318f3809038590af7ec",
+    "url": "/test-app/static/js/runtime~main.e9adfd5a.js"
+  },
+  {
+    "revision": "ee51fa55b80f67395d22",
+    "url": "/test-app/static/js/main.f69899f4.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/test-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ee51fa55b80f67395d22",
+    "url": "/test-app/static/css/main.f195b5c6.chunk.css"
+  },
+  {
+    "revision": "f935dca8fb55e471639bc00f4734dd14",
+    "url": "/test-app/index.html"
+  }
+];
